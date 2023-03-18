5bb15dd69bf36e72a0feaadc5c3f852e51e07df5
layan eid <layanalshl77@gmail.com>
2023-03-18 16:37:04 +0300

    layan commit

---
+++ src/main/java/com/mycompany/layan_group6/Layan_group6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.layan_group6;
+
+/**
+ *
+ * @author HP
+ */
+public class Layan_group6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+    }
+}
